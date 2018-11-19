a101e12a911ec1060912b75115ae41163c45b4de
unknown <jiadongjie>
2018-11-19 16:20:16 +0800

    东杰改

--- Attorney/app/Http/Controllers/HomeController.php
+++ Attorney/app/Http/Controllers/HomeController.php
@@ -5,12 +5,10 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(){
-        echo '123';
         return view('Home.home');
     }
 
     public function homes(){
-        echo '124';
         return view('Home.homes');
     }
 
